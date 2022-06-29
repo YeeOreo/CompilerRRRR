9460e1f85618feac10e62157d20fd450b9b82565
Jun3631 <72019035+Jun3631@users.noreply.github.com>
2022-06-29 19:14:02 +0800

    Add files via upload

--- compile_and_run.bat
+++ compile_and_run.bat
@@ -1,7 +1,14 @@
-set arg1=%1
+setlocal
+set arg1=calculator_trail
 shift
 flex %arg1%.l
 bison -d %arg1%.y
 gcc %arg1%.tab.c lex.yy.c -lfl 
+@echo off
+cls
+:start
 .\a.exe
-PAUSE
+set choice=
+set /p choice="Do you want to restart? Press 'y' and enter for Yes: "
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='y' goto start
